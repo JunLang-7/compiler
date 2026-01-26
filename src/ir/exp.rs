03ef75fc80cab75d2935c0951cd0ba1732a7f384
junlang-7 <1448760937@qq.com>
2026-01-26 05:35:09 +0000

    Refactor: encapsulate the args and move function args FunctionData & HashMap to GenContext sturct

--- src/ir/exp.rs
+++ src/ir/exp.rs
@@ -1,47 +1,37 @@
-use super::Symbol;
+use super::{GenContext, Symbol};
 use crate::ast::*;
 use koopa::ir::builder_traits::*;
-use koopa::ir::{BasicBlock, BinaryOp, FunctionData, Value};
-use std::collections::HashMap;
+use koopa::ir::{BasicBlock, BinaryOp, Value};
 
 /// Generate Koopa IR for an expression
-pub fn generate_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    exp: &Exp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
-    generate_lor_exp(func_data, bb, &exp.lor_exp, symbol_table)
+pub fn generate_exp(ctx: &mut GenContext, bb: BasicBlock, exp: &Exp) -> Value {
+    generate_lor_exp(ctx, bb, &exp.lor_exp)
 }
 
 /// Generate Koopa IR for a unary expression
-pub fn generate_unary_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    unary_exp: &UnaryExp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
+pub fn generate_unary_exp(ctx: &mut GenContext, bb: BasicBlock, unary_exp: &UnaryExp) -> Value {
     match unary_exp {
-        UnaryExp::PrimaryExp(primary_exp) => {
-            generate_primary_exp(func_data, bb, primary_exp, symbol_table)
-        }
+        UnaryExp::PrimaryExp(primary_exp) => generate_primary_exp(ctx, bb, primary_exp),
         UnaryExp::UnaryOp { op, exp } => {
-            let val = generate_unary_exp(func_data, bb, exp, symbol_table);
-            let zero = func_data.dfg_mut().new_value().integer(0);
+            let val = generate_unary_exp(ctx, bb, exp);
+            let zero = ctx.func_data.dfg_mut().new_value().integer(0);
             let result = match op {
                 UnaryOp::Plus => val,
-                UnaryOp::Minus => func_data
-                    .dfg_mut()
-                    .new_value()
-                    .binary(BinaryOp::Sub, zero, val),
-                UnaryOp::Not => func_data
+                UnaryOp::Minus => {
+                    ctx.func_data
+                        .dfg_mut()
+                        .new_value()
+                        .binary(BinaryOp::Sub, zero, val)
+                }
+                UnaryOp::Not => ctx
+                    .func_data
                     .dfg_mut()
                     .new_value()
                     .binary(BinaryOp::Eq, zero, val),
             };
             // 如果不是加号操作符, 则需要将结果加入到基本块中
             if !matches!(op, UnaryOp::Plus) {
-                func_data
+                ctx.func_data
                     .layout_mut()
                     .bb_mut(bb)
                     .insts_mut()
@@ -55,22 +45,21 @@ pub fn generate_unary_exp(
 
 /// Generate Koopa IR for a primary expression
 pub fn generate_primary_exp(
-    func_data: &mut FunctionData,
+    ctx: &mut GenContext,
     bb: BasicBlock,
     primary_exp: &PrimaryExp,
-    symbol_table: &mut HashMap<String, Symbol>,
 ) -> Value {
     match primary_exp {
-        PrimaryExp::Exp(exp) => generate_exp(func_data, bb, exp, symbol_table),
-        PrimaryExp::Number(num) => func_data.dfg_mut().new_value().integer(*num),
+        PrimaryExp::Exp(exp) => generate_exp(ctx, bb, exp),
+        PrimaryExp::Number(num) => ctx.func_data.dfg_mut().new_value().integer(*num),
         PrimaryExp::LVal(lval) => {
-            if let Some(&symbol) = symbol_table.get(&lval.ident) {
+            if let Some(&symbol) = ctx.symbol_table.get(&lval.ident) {
                 match symbol {
-                    Symbol::Const(val) => func_data.dfg_mut().new_value().integer(val),
+                    Symbol::Const(val) => ctx.func_data.dfg_mut().new_value().integer(val),
                     Symbol::Var(val) => {
                         // 取出变量的值
-                        let load = func_data.dfg_mut().new_value().load(val);
-                        func_data
+                        let load = ctx.func_data.dfg_mut().new_value().load(val);
+                        ctx.func_data
                             .layout_mut()
                             .bb_mut(bb)
                             .insts_mut()
@@ -87,32 +76,27 @@ pub fn generate_primary_exp(
 }
 
 /// Generate Koopa IR for an additive expression
-pub fn generate_add_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    add_exp: &AddExp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
+pub fn generate_add_exp(ctx: &mut GenContext, bb: BasicBlock, add_exp: &AddExp) -> Value {
     match add_exp {
-        AddExp::MulExp(mul_exp) => generate_mul_exp(func_data, bb, mul_exp, symbol_table),
+        AddExp::MulExp(mul_exp) => generate_mul_exp(ctx, bb, mul_exp),
         AddExp::AddOp { lhs, op, rhs } => {
-            let lhs_val = generate_add_exp(func_data, bb, lhs, symbol_table);
-            let rhs_val = generate_mul_exp(func_data, bb, rhs, symbol_table);
+            let lhs_val = generate_add_exp(ctx, bb, lhs);
+            let rhs_val = generate_mul_exp(ctx, bb, rhs);
             let result = match op {
                 AddOp::Plus => {
-                    func_data
+                    ctx.func_data
                         .dfg_mut()
                         .new_value()
                         .binary(BinaryOp::Add, lhs_val, rhs_val)
                 }
                 AddOp::Minus => {
-                    func_data
+                    ctx.func_data
                         .dfg_mut()
                         .new_value()
                         .binary(BinaryOp::Sub, lhs_val, rhs_val)
                 }
             };
-            func_data
+            ctx.func_data
                 .layout_mut()
                 .bb_mut(bb)
                 .insts_mut()
@@ -124,38 +108,33 @@ pub fn generate_add_exp(
 }
 
 /// Generate Koopa IR for a multiplicative expression
-pub fn generate_mul_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    mul_exp: &MulExp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
+pub fn generate_mul_exp(ctx: &mut GenContext, bb: BasicBlock, mul_exp: &MulExp) -> Value {
     match mul_exp {
-        MulExp::UnaryExp(unary_exp) => generate_unary_exp(func_data, bb, unary_exp, symbol_table),
+        MulExp::UnaryExp(unary_exp) => generate_unary_exp(ctx, bb, unary_exp),
         MulExp::MulOp { lhs, op, rhs } => {
-            let lhs_val = generate_mul_exp(func_data, bb, lhs, symbol_table);
-            let rhs_val = generate_unary_exp(func_data, bb, rhs, symbol_table);
+            let lhs_val = generate_mul_exp(ctx, bb, lhs);
+            let rhs_val = generate_unary_exp(ctx, bb, rhs);
             let result = match op {
                 MulOp::Mul => {
-                    func_data
+                    ctx.func_data
                         .dfg_mut()
                         .new_value()
                         .binary(BinaryOp::Mul, lhs_val, rhs_val)
                 }
                 MulOp::Div => {
-                    func_data
+                    ctx.func_data
                         .dfg_mut()
                         .new_value()
                         .binary(BinaryOp::Div, lhs_val, rhs_val)
                 }
                 MulOp::Mod => {
-                    func_data
+                    ctx.func_data
                         .dfg_mut()
                         .new_value()
                         .binary(BinaryOp::Mod, lhs_val, rhs_val)
                 }
             };
-            func_data
+            ctx.func_data
                 .layout_mut()
                 .bb_mut(bb)
                 .insts_mut()
@@ -167,36 +146,39 @@ pub fn generate_mul_exp(
 }
 
 /// Generate Koopa IR for a relational expression
-pub fn generate_rel_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    rel_exp: &RelExp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
+pub fn generate_rel_exp(ctx: &mut GenContext, bb: BasicBlock, rel_exp: &RelExp) -> Value {
     match rel_exp {
-        RelExp::AddExp(add_exp) => generate_add_exp(func_data, bb, add_exp, symbol_table),
+        RelExp::AddExp(add_exp) => generate_add_exp(ctx, bb, add_exp),
         RelExp::RelOp { lhs, op, rhs } => {
-            let lhs_val = generate_rel_exp(func_data, bb, lhs, symbol_table);
-            let rhs_val = generate_add_exp(func_data, bb, rhs, symbol_table);
+            let lhs_val = generate_rel_exp(ctx, bb, lhs);
+            let rhs_val = generate_add_exp(ctx, bb, rhs);
             let result = match op {
-                RelOp::Lt => func_data
-                    .dfg_mut()
-                    .new_value()
-                    .binary(BinaryOp::Lt, lhs_val, rhs_val),
-                RelOp::Gt => func_data
-                    .dfg_mut()
-                    .new_value()
-                    .binary(BinaryOp::Gt, lhs_val, rhs_val),
-                RelOp::Le => func_data
-                    .dfg_mut()
-                    .new_value()
-                    .binary(BinaryOp::Le, lhs_val, rhs_val),
-                RelOp::Ge => func_data
-                    .dfg_mut()
-                    .new_value()
-                    .binary(BinaryOp::Ge, lhs_val, rhs_val),
+                RelOp::Lt => {
+                    ctx.func_data
+                        .dfg_mut()
+                        .new_value()
+                        .binary(BinaryOp::Lt, lhs_val, rhs_val)
+                }
+                RelOp::Gt => {
+                    ctx.func_data
+                        .dfg_mut()
+                        .new_value()
+                        .binary(BinaryOp::Gt, lhs_val, rhs_val)
+                }
+                RelOp::Le => {
+                    ctx.func_data
+                        .dfg_mut()
+                        .new_value()
+                        .binary(BinaryOp::Le, lhs_val, rhs_val)
+                }
+                RelOp::Ge => {
+                    ctx.func_data
+                        .dfg_mut()
+                        .new_value()
+                        .binary(BinaryOp::Ge, lhs_val, rhs_val)
+                }
             };
-            func_data
+            ctx.func_data
                 .layout_mut()
                 .bb_mut(bb)
                 .insts_mut()
@@ -208,30 +190,27 @@ pub fn generate_rel_exp(
 }
 
 /// Generate Koopa IR for an equality expression
-pub fn generate_eq_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    eq_exp: &EqExp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
+pub fn generate_eq_exp(ctx: &mut GenContext, bb: BasicBlock, eq_exp: &EqExp) -> Value {
     match eq_exp {
-        EqExp::RelExp(rel_exp) => generate_rel_exp(func_data, bb, rel_exp, symbol_table),
+        EqExp::RelExp(rel_exp) => generate_rel_exp(ctx, bb, rel_exp),
         EqExp::EqOp { lhs, op, rhs } => {
-            let lhs_val = generate_eq_exp(func_data, bb, lhs, symbol_table);
-            let rhs_val = generate_rel_exp(func_data, bb, rhs, symbol_table);
+            let lhs_val = generate_eq_exp(ctx, bb, lhs);
+            let rhs_val = generate_rel_exp(ctx, bb, rhs);
             let result = match op {
-                EqOp::Eq => func_data
-                    .dfg_mut()
-                    .new_value()
-                    .binary(BinaryOp::Eq, lhs_val, rhs_val),
+                EqOp::Eq => {
+                    ctx.func_data
+                        .dfg_mut()
+                        .new_value()
+                        .binary(BinaryOp::Eq, lhs_val, rhs_val)
+                }
                 EqOp::Ne => {
-                    func_data
+                    ctx.func_data
                         .dfg_mut()
                         .new_value()
                         .binary(BinaryOp::NotEq, lhs_val, rhs_val)
                 }
             };
-            func_data
+            ctx.func_data
                 .layout_mut()
                 .bb_mut(bb)
                 .insts_mut()
@@ -243,31 +222,29 @@ pub fn generate_eq_exp(
 }
 
 /// Generate Koopa IR for a logical AND expression
-pub fn generate_land_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    land_exp: &LAndExp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
+pub fn generate_land_exp(ctx: &mut GenContext, bb: BasicBlock, land_exp: &LAndExp) -> Value {
     match land_exp {
-        LAndExp::EqExp(eq_exp) => generate_eq_exp(func_data, bb, eq_exp, symbol_table),
+        LAndExp::EqExp(eq_exp) => generate_eq_exp(ctx, bb, eq_exp),
         LAndExp::LAndOp { lhs, rhs } => {
-            let lhs_val = generate_land_exp(func_data, bb, lhs, symbol_table);
-            let rhs_val = generate_eq_exp(func_data, bb, rhs, symbol_table);
-            let zero = func_data.dfg_mut().new_value().integer(0);
-            let lhs_ne0 = func_data
-                .dfg_mut()
-                .new_value()
-                .binary(BinaryOp::NotEq, lhs_val, zero);
-            let rhs_ne0 = func_data
-                .dfg_mut()
-                .new_value()
-                .binary(BinaryOp::NotEq, rhs_val, zero);
-            let result = func_data
-                .dfg_mut()
-                .new_value()
-                .binary(BinaryOp::And, lhs_ne0, rhs_ne0);
-            func_data
+            let lhs_val = generate_land_exp(ctx, bb, lhs);
+            let rhs_val = generate_eq_exp(ctx, bb, rhs);
+            let zero = ctx.func_data.dfg_mut().new_value().integer(0);
+            let lhs_ne0 =
+                ctx.func_data
+                    .dfg_mut()
+                    .new_value()
+                    .binary(BinaryOp::NotEq, lhs_val, zero);
+            let rhs_ne0 =
+                ctx.func_data
+                    .dfg_mut()
+                    .new_value()
+                    .binary(BinaryOp::NotEq, rhs_val, zero);
+            let result =
+                ctx.func_data
+                    .dfg_mut()
+                    .new_value()
+                    .binary(BinaryOp::And, lhs_ne0, rhs_ne0);
+            ctx.func_data
                 .layout_mut()
                 .bb_mut(bb)
                 .insts_mut()
@@ -278,31 +255,29 @@ pub fn generate_land_exp(
 }
 
 /// Generate Koopa IR for a logical OR expression
-pub fn generate_lor_exp(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    lor_exp: &LOrExp,
-    symbol_table: &mut HashMap<String, Symbol>,
-) -> Value {
+pub fn generate_lor_exp(ctx: &mut GenContext, bb: BasicBlock, lor_exp: &LOrExp) -> Value {
     match lor_exp {
-        LOrExp::LAndExp(land_exp) => generate_land_exp(func_data, bb, land_exp, symbol_table),
+        LOrExp::LAndExp(land_exp) => generate_land_exp(ctx, bb, land_exp),
         LOrExp::LOrOp { lhs, rhs } => {
-            let lhs_val = generate_lor_exp(func_data, bb, lhs, symbol_table);
-            let rhs_val = generate_land_exp(func_data, bb, rhs, symbol_table);
-            let zero = func_data.dfg_mut().new_value().integer(0);
-            let lhs_ne0 = func_data
-                .dfg_mut()
-                .new_value()
-                .binary(BinaryOp::NotEq, lhs_val, zero);
-            let rhs_ne0 = func_data
-                .dfg_mut()
-                .new_value()
-                .binary(BinaryOp::NotEq, rhs_val, zero);
-            let result = func_data
+            let lhs_val = generate_lor_exp(ctx, bb, lhs);
+            let rhs_val = generate_land_exp(ctx, bb, rhs);
+            let zero = ctx.func_data.dfg_mut().new_value().integer(0);
+            let lhs_ne0 =
+                ctx.func_data
+                    .dfg_mut()
+                    .new_value()
+                    .binary(BinaryOp::NotEq, lhs_val, zero);
+            let rhs_ne0 =
+                ctx.func_data
+                    .dfg_mut()
+                    .new_value()
+                    .binary(BinaryOp::NotEq, rhs_val, zero);
+            let result = ctx
+                .func_data
                 .dfg_mut()
                 .new_value()
                 .binary(BinaryOp::Or, lhs_ne0, rhs_ne0);
-            func_data
+            ctx.func_data
                 .layout_mut()
                 .bb_mut(bb)
                 .insts_mut()
--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -15,49 +15,53 @@ pub enum Symbol {
     Var(Value),
 }
 
+/// Context for Koopa IR generation
+pub struct GenContext<'a> {
+    func_data: &'a mut FunctionData,
+    symbol_table: HashMap<String, Symbol>,
+}
+
 /// Generate Koopa IR from the AST
 pub fn generate_koopa(ast: &ast::CompUnit) -> Program {
     // create the program and function
     let mut program = Program::new();
     let func_data = FunctionData::new(format!("@{}", ast.func_def.ident), vec![], Type::get_i32());
     let func = program.new_func(func_data);
-    let func_data = program.func_mut(func);
-    let mut symbol_table: HashMap<String, Symbol> = HashMap::new();
+    let mut ctx = GenContext {
+        func_data: program.func_mut(func),
+        symbol_table: HashMap::new(),
+    };
 
     // 在函数中创建一个基本块(entry basic block)
-    let entry = func_data
+    let entry = ctx
+        .func_data
         .dfg_mut()
         .new_bb()
         .basic_block(Some("%entry".into()));
     // 将基本块添加到函数布局中
-    func_data
+    ctx.func_data
         .layout_mut()
         .bbs_mut()
         .push_key_back(entry)
         .expect("fail to push entry bb");
     // 生成函数体的中间表示
-    generate_block(func_data, entry, &ast.func_def.block, &mut symbol_table);
+    generate_block(&mut ctx, entry, &ast.func_def.block);
 
     program
 }
 
 /// Generate Koopa IR for a block
-pub fn generate_block(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    block: &Block,
-    symbol_table: &mut HashMap<String, Symbol>,
-) {
+pub fn generate_block(ctx: &mut GenContext, bb: BasicBlock, block: &Block) {
     for block_item in &block.block_items {
         match block_item {
             BlockItem::Stmt(stmt) => {
                 match stmt {
                     Stmt::Return(exp) => {
                         // 生成语句的中间表示
-                        let ret_val = generate_exp(func_data, bb, exp, symbol_table);
-                        let ret = func_data.dfg_mut().new_value().ret(Some(ret_val));
+                        let ret_val = generate_exp(ctx, bb, exp);
+                        let ret = ctx.func_data.dfg_mut().new_value().ret(Some(ret_val));
                         // 将返回语句添加到基本块中
-                        func_data
+                        ctx.func_data
                             .layout_mut()
                             .bb_mut(bb)
                             .insts_mut()
@@ -65,7 +69,7 @@ pub fn generate_block(
                             .expect("fail to push return value");
                     }
                     Stmt::LValAssign { lval, exp } => {
-                        let var = if let Some(symbol) = symbol_table.get(&lval.ident) {
+                        let var = if let Some(symbol) = ctx.symbol_table.get(&lval.ident) {
                             match symbol {
                                 Symbol::Const(_) => unreachable!(),
                                 Symbol::Var(var) => *var,
@@ -74,10 +78,10 @@ pub fn generate_block(
                             panic!("Cannot assign a undefined variable!");
                         };
                         // 生成表达式的结果
-                        let res = generate_exp(func_data, bb, exp, symbol_table);
+                        let res = generate_exp(ctx, bb, exp);
                         // 将结果存入symbol table
-                        let store = func_data.dfg_mut().new_value().store(res, var);
-                        func_data
+                        let store = ctx.func_data.dfg_mut().new_value().store(res, var);
+                        ctx.func_data
                             .layout_mut()
                             .bb_mut(bb)
                             .insts_mut()
@@ -87,43 +91,40 @@ pub fn generate_block(
                 }
             }
             BlockItem::Decl(decl) => {
-                generate_decl(func_data, bb, &decl, symbol_table);
+                generate_decl(ctx, bb, &decl);
             }
         }
     }
 }
 
 /// Generate Koopa IR for a declaration
-pub fn generate_decl(
-    func_data: &mut FunctionData,
-    bb: BasicBlock,
-    decl: &Decl,
-    symbol_table: &mut HashMap<String, Symbol>,
-) {
+pub fn generate_decl(ctx: &mut GenContext, bb: BasicBlock, decl: &Decl) {
     match decl {
         Decl::ConstDecl(const_decl) => {
             for def in &const_decl.const_defs {
                 let exp = &def.const_init_val.const_exp.exp;
-                let value = exp.evaluate(symbol_table);
-                symbol_table.insert(def.ident.clone(), Symbol::Const(value));
+                let value = exp.evaluate(&mut ctx.symbol_table);
+                ctx.symbol_table
+                    .insert(def.ident.clone(), Symbol::Const(value));
             }
         }
         Decl::VarDecl(var_decl) => {
             for def in &var_decl.var_defs {
                 // 在栈上分配内存
-                let alloc = func_data.dfg_mut().new_value().alloc(Type::get_i32());
-                func_data
+                let alloc = ctx.func_data.dfg_mut().new_value().alloc(Type::get_i32());
+                ctx.func_data
                     .layout_mut()
                     .bb_mut(bb)
                     .insts_mut()
                     .push_key_back(alloc)
                     .expect("failed to add alloc instruction");
-                symbol_table.insert(def.ident.clone(), Symbol::Var(alloc));
+                ctx.symbol_table
+                    .insert(def.ident.clone(), Symbol::Var(alloc));
                 // 处理初始化值
                 if let Some(init_val) = &def.init_val {
-                    let res = generate_exp(func_data, bb, &init_val.exp, symbol_table);
-                    let store = func_data.dfg_mut().new_value().store(res, alloc);
-                    func_data
+                    let res = generate_exp(ctx, bb, &init_val.exp);
+                    let store = ctx.func_data.dfg_mut().new_value().store(res, alloc);
+                    ctx.func_data
                         .layout_mut()
                         .bb_mut(bb)
                         .insts_mut()
